8f7c7516f4aa3547d3a821a3a70fbe15183d0421
Admin <admin@mac-admin.local>
2016-08-12 18:43:28 +0500

    TableView

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Admin on 12.08.16.
+//  Copyright © 2016 house. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+}
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -33,4 +33,17 @@ class FoodTrackerTests: XCTestCase {
         }
     }
     
+    //MARK: FoodTracker Tests
+    func testMealInitialization() {
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        
+        //Failure cases
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating, "Negative ratings are invalid")
+    }
+    
 }
